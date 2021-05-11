3c2f9aa07a5050e431e0c30e963496c4e8f42f27
anurag-shekhar <anurag.shekhar.work@gmail.com>
2021-05-11 15:59:50 +0530

    google telephonic interview

--- Leetcode/Problems/1314.MatrixBlockSum/MatrixBlockSum.cpp
+++ Leetcode/Problems/1314.MatrixBlockSum/MatrixBlockSum.cpp
@@ -1,3 +1,16 @@
+/*
+ ****************************************************************
+ * Author : Anurag Shekhar
+ * Email : anurag.shekhar.work@gmail.com
+ *
+ * Github : https://github.com/anurag-shekhar
+ * LinkedIn : https://www.linkedin.com/in/anurag-shekhar/
+ *
+ * File Name : MatrixBlockSum.cpp
+ * Created on : Tue May 11 2021
+ ****************************************************************
+ */
+
 class Solution {
 public:
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
--- Leetcode/Problems/989.AddToArrayFormOfInteger/AddToArrayFormOfInteger.cpp
+++ Leetcode/Problems/989.AddToArrayFormOfInteger/AddToArrayFormOfInteger.cpp
@@ -1,3 +1,16 @@
+/*
+ ****************************************************************
+ * Author : Anurag Shekhar
+ * Email : anurag.shekhar.work@gmail.com
+ *
+ * Github : https://github.com/anurag-shekhar
+ * LinkedIn : https://www.linkedin.com/in/anurag-shekhar/
+ *
+ * File Name : AddToArrayFormOfInteger.cpp
+ * Created on : Tue May 11 2021
+ ****************************************************************
+ */
+
 class Solution {
 public:
     vector<int> addToArrayForm(vector<int>& num, int k) {

09202ae6ca8e74f0d594157a70e5686b1b96ebd1
anurag-shekhar <anurag.shekhar.work@gmail.com>
2021-05-29 18:28:18 +0530

    May Week5 Bonus

--- Leetcode/Problems/1167.MinimumCostToConnectSticks/MinimumCostToConnectSticks.cpp
+++ Leetcode/Problems/1167.MinimumCostToConnectSticks/MinimumCostToConnectSticks.cpp
@@ -1,3 +1,16 @@
+/*
+ ****************************************************************
+ * Author : Anurag Shekhar
+ * Email : anurag.shekhar.work@gmail.com
+ *
+ * Github : https://github.com/anurag-shekhar
+ * LinkedIn : https://www.linkedin.com/in/anurag-shekhar/
+ *
+ * File Name : MinimumCostToConnectSticks.cpp
+ * Created on : Sat May 29 2021
+ ****************************************************************
+ */
+
 class Solution {
 public:
     int connectSticks(vector<int>& sticks) {

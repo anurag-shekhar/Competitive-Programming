565fbdc03bc451360fa951e62b9d5ec6d48c5e17
anurag-shekhar <anurag.shekhar.work@gmail.com>
2021-06-14 08:16:41 +0530

    LC Problems

---
+++ Leetcode/Problems/141.LinkedListCycle/LinkedListCycle.cpp
@@ -0,0 +1,39 @@
+/*
+ ****************************************************************
+ * Author : Anurag Shekhar
+ * Email : anurag.shekhar.work@gmail.com
+ *
+ * Github : https://github.com/anurag-shekhar
+ * LinkedIn : https://www.linkedin.com/in/anurag-shekhar/
+ *
+ * File Name : LinkedListCycle.cpp
+ * Created on : Mon Jun 14 2021
+ ****************************************************************
+ */
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while(fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(fast == slow)
+                return true;
+        }
+        return false;
+    }
+};

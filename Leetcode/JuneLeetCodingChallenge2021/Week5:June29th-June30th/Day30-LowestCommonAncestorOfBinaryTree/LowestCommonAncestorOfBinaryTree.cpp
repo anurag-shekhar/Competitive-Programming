6a029cda57008fec565aa49819e006a9bd959c18
anurag-shekhar <anurag.shekhar.work@gmail.com>
2021-06-30 14:07:21 +0530

    June Day 30

---
+++ Leetcode/JuneLeetCodingChallenge2021/Week5:June29th-June30th/Day30-LowestCommonAncestorOfBinaryTree/LowestCommonAncestorOfBinaryTree.cpp
@@ -0,0 +1,42 @@
+/*
+ ****************************************************************
+ * Author : Anurag Shekhar
+ * Email : anurag.shekhar.work@gmail.com
+ *
+ * Github : https://github.com/anurag-shekhar
+ * LinkedIn : https://www.linkedin.com/in/anurag-shekhar/
+ *
+ * File Name : LowestCommonAncestorOfBinaryTree.cpp
+ * Created on : Wed Jun 30 2021
+ ****************************************************************
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        if(!root)
+            return NULL;
+        if(root == p || root == q)
+            return root;
+        
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        
+        if(left && right)
+            return root;
+        else if(left)
+            return left;
+        return right;
+        
+    }
+};

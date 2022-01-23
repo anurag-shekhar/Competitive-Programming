08144dfd0f2767b25101a9b29bf8498e746def4b
anurag-shekhar <anurag.shekhar.work@gmail.com>
2022-01-23 16:38:55 +0530

    Algo Day8

---
+++ Leetcode/Problems/617.MergeTwoBinaryTrees/MergeTwoBinaryTrees.cpp
@@ -0,0 +1,41 @@
+/*
+ ****************************************************************
+ * Author : Anurag Shekhar
+ * Email : anurag.shekhar.work@gmail.com
+ *
+ * Github : https://github.com/anurag-shekhar
+ * LinkedIn : https://www.linkedin.com/in/anurag-shekhar/
+ *
+ * File Name : MergeTwoBinaryTrees.cpp
+ * Created on : Sun Jan 23 2022
+ ****************************************************************
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        if(!root1 && !root2)
+            return NULL;
+        if(!root1)
+            return root2;
+        if(!root2)
+            return root1;
+        root1->val += root2->val;
+        root1->left = mergeTrees(root1->left, root2->left);
+        root1->right = mergeTrees(root1->right, root2->right);
+        return root1;
+        
+        
+    }
+};

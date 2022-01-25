f70267ca341744f5fc3ecdf8d36d06f19919aeb3
anurag-shekhar <anurag.shekhar.work@gmail.com>
2022-01-25 18:42:34 +0530

    DS1 - Day6

---
+++ Leetcode/StudyPlan/DataStructure-I/Day6-String/242.ValidAnagram/ValidAnagram.cpp
@@ -0,0 +1,21 @@
+/*
+ ****************************************************************
+ * Author : Anurag Shekhar
+ * Email : anurag.shekhar.work@gmail.com
+ *
+ * Github : https://github.com/anurag-shekhar
+ * LinkedIn : https://www.linkedin.com/in/anurag-shekhar/
+ *
+ * File Name : ValidAnagram.cpp
+ * Created on : Tue Jan 25 2022
+ ****************************************************************
+ */
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s==t;
+    }
+};
